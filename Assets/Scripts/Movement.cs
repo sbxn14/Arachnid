e21512dda657d6b6832600041b56b49178da759c
Sander Berntsen <sander1006@hotmail.nl>
2020-10-09 12:27:37 +0200

    Fixed git finally

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 10f;
+    [SerializeField]
+    float rotationSensitivity = 90f;
+    [SerializeField]
+    float rotationSpeed = 90f;
+
+    Vector3 inputVelocity, localInput, worldVelocity;
+
+    [SerializeField]
+    float rSpeed = 0f;
+
+    [SerializeField]
+    ProcedurallyAnimatedLeg[] legs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localInput = Vector3.ClampMagnitude(transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"))), 1f);
+        inputVelocity = Vector3.MoveTowards(inputVelocity, localInput, Time.deltaTime * 2f);
+        worldVelocity = inputVelocity * moveSpeed;
+
+        transform.position += (worldVelocity * Time.deltaTime);
+
+        rSpeed = Mathf.MoveTowards(rSpeed, Input.GetAxis("Turn") * rotationSpeed, rotationSensitivity * Time.deltaTime);
+
+        transform.Rotate(0f, rSpeed * Time.deltaTime, 0f);
+
+        for (int i = 0; i < legs.Length; i++)
+        {
+            //legs[i].SetVelocity(CalculateLegVelocity(i));
+        }
+    }
+
+    Vector3 CalculateLegVelocity(int index)
+    {
+        Vector3 legPoint = legs[index].restPosition;
+        Vector3 direction = legPoint - transform.position;
+
+        //Vector3 rotationalPoint = ((Quaternion.AngleAxis((rSpeed * timeBetweenSteps) / 2f, transform.up) * direction) + transform.position) - legPoint;
+
+        //return rotationalPoint + (worldVelocity * timeBetweenSteps) / 2f;
+        return worldVelocity;
+    }
+}
---
+++ Assets/Scripts/ProcedurallyAnimatedLeg.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcedurallyAnimatedLeg : MonoBehaviour
+{
+    [SerializeField]
+    bool isGrounded = false;
+
+    [SerializeField]
+    Transform target, pole;
+    [SerializeField]
+    Transform bodyTarget;
+    [SerializeField]
+    float stepRadius = .25f;
+    [SerializeField]
+    LayerMask probeMask;
+
+    [SerializeField]
+    float distanceBetweenTargets = 0f;
+
+    public Vector3 restPosition;
+
+    Vector3 stepPosition, stepNormal;
+
+    Vector3 worldVelocity;
+
+    private void Start()
+    {
+        restPosition = bodyTarget.position;
+    }
+
+    void Update()
+    {
+        SphereCast(restPosition + worldVelocity);
+        CalculateDistanceBetweenTargets();
+        UpdateTarget(stepPosition);
+    }
+
+    private void LateUpdate()
+    {
+        if (distanceBetweenTargets > 1.5f)
+        {
+            UpdateTarget(bodyTarget.position);
+            restPosition = target.position;
+        }
+        AdjustNormals();
+    }
+
+    void CalculateDistanceBetweenTargets()
+    {
+        distanceBetweenTargets = Vector3.Distance(target.position, bodyTarget.position);
+    }
+
+    void SphereCast(Vector3 position)
+    {
+        Vector3 direction = position - pole.position;
+        RaycastHit hit;
+
+        if (Physics.SphereCast(pole.position, stepRadius, direction, out hit, direction.magnitude * 2f, probeMask))
+        {
+            //Debug.DrawLine(pole.position, hit.point, Color.green);
+            stepPosition = hit.point;
+            stepNormal = hit.normal;
+            isGrounded = true;
+        }
+        else
+        {
+            //Debug.DrawLine(pole.position, restPosition, Color.red);
+            stepPosition = restPosition;
+            stepNormal = Vector3.zero;
+            isGrounded = false;
+        }
+    }
+
+    void AdjustNormals()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(bodyTarget.position, -stepNormal, out hit, probeMask))
+        {
+            Vector3 newPos = bodyTarget.position;
+            newPos.y = hit.point.y;
+            //bodyTarget.position = Vector3.Lerp(bodyTarget.position, newPos, 1f);
+
+            bodyTarget.position = Vector3.Lerp(bodyTarget.position, hit.point, 1f);
+        }
+    }
+
+    void UpdateTarget(Vector3 position)
+    {
+        target.position = Vector3.Lerp(target.position, position, 1f);
+    }
+
+    public void SetVelocity(Vector3 newVelocity)
+    {
+        worldVelocity = Vector3.Lerp(worldVelocity, newVelocity, 1f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(pole.position, target.position);
+        Gizmos.DrawSphere(target.position, .15f);
+        Gizmos.DrawSphere(pole.position, .15f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(bodyTarget.position, .15f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(target.position, bodyTarget.position);
+    }
+}
